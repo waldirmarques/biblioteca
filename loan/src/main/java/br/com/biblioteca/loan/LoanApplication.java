f8a9c76eb8cf71eb9f5ab0d5d050719f74c886e0
waldir.sousa <waldir.sousa@phoebustecnologia.com.br>
2020-04-16 17:49:57 -0300

    changes project

--- loan/src/main/java/br/com/biblioteca/loan/LoanApplication.java
+++ loan/src/main/java/br/com/biblioteca/loan/LoanApplication.java
@@ -3,9 +3,11 @@ package br.com.biblioteca.loan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class LoanApplication {
 
 	public static void main(String[] args) {
